24949f92a9ef54bd4cd0c1fa857051c6836795a9
ericholsinger <eric.holsinger@gmail.com>
2024-10-13 11:30:34 -0400

    blank spring boot

---
+++ encounter-builder/src/main/java/com/dmpreroll/app/encounterbuilder/EncounterBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.dmpreroll.app.encounterbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncounterBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncounterBuilderApplication.class, args);
+	}
+
+}
